3a682d13a38dea6ae610115536b1b4263a83bb3d
Denis Diachkov <d.diachkov@gmail.com>
2013-03-15 17:59:51 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Sublime Text 2 plugin: YAML Nav
+
+Simple plugin for quick navigating in YAML files.
+
+![screenshot][screenshot]
+
+## Installation
+
+ * cd <YOUR PACKAGES DIRECTORY> (eg. ~/Library/Application\ Support/Sublime\ Text\ 2/Packages)
+ * git clone http://github.com/ddiachkov/sublime-yaml-nav
+
+## Key bindings
+
+Command + r (Control + r) in YAML file will run "goto_yaml_symbol" command.
+
+## Licence
+
+All of YAML Nav is licensed under the MIT licence.
+
+  Copyright (c) 2013 Denis Diachkov
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
---
+++ yaml_nav.py
@@ -0,0 +1,46 @@
+import sublime, sublime_plugin
+
+class GotoYamlSymbolCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.view = self.window.active_view()
+        self.symbols = self.get_yaml_symbols()
+        self.window.show_quick_panel(map(lambda x: x["name"], self.symbols), self.on_symbol_selected)
+
+    def on_symbol_selected(self, index):
+        if index >= 0:
+            region = self.symbols[index]["region"]
+
+            self.view.show_at_center(region)
+
+            # Set cursor after YAML key
+            self.view.sel().clear()
+            self.view.sel().add(sublime.Region(region.end() + 1))
+
+    def get_yaml_symbols(self):
+        """
+        Returns YAML keys paths.
+        Paths calculated by key indentation level -- it's more efficient and secure, but doesn't support inline hashes.
+        """
+
+        regions = self.view.find_by_selector("entity.name.tag.yaml")
+
+        symbols = []
+        current_path = []
+
+        for region in regions:
+            key = self.view.substr(region).rstrip(":")
+            line = self.view.line(region)
+
+            # Character count from line beginning to key start position
+            ident_level = region.begin() - line.begin()
+
+            # Pop items from current_path while its indentation level less than current key indentation
+            while len(current_path) > 0 and current_path[-1][ "ident" ] >= ident_level:
+              current_path.pop()
+
+            current_path.append({"key": key, "ident": ident_level})
+
+            symbol_name = ".".join(map(lambda item: item["key"], current_path))
+            symbols.append({"name": symbol_name, "region": region})
+
+        return symbols
